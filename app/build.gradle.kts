b1c45e694c2eee6260456a4a867bbdd6b843d5a6
Miroslav Hýbler <miroslav@peko-studio.cz>
2024-12-11 12:37:44 +0100

    HOTFIX

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "com.jet.article.example.devblog"
         minSdk = 24
         targetSdk = 35
-        versionCode = 8
+        versionCode = 9
         versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
