0b5441335d7c7201a150d48b833c61a4d52f3bf7
Miroslav Hýbler <miroslav@peko-studio.cz>
2025-01-12 21:10:38 +0100

    Updated jet-article library to fix compose compiler IR lowering build crash

--- app/src/main/kotlin/com/jet/article/example/devblog/composables/CustomHtmlImage.kt
+++ app/src/main/kotlin/com/jet/article/example/devblog/composables/CustomHtmlImage.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
